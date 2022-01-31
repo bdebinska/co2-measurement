743455d49e8dbd18148c41783642cd75ed523d58
Beata Debinska <bea.debinska@gmail.com>
2022-01-31 09:16:01 +0100

    Fix bug

--- server/index.js
+++ server/index.js
@@ -72,12 +72,12 @@ app.get("/api", (req, res) => {
   });
 });
 
-app.use(express.static(path.resolve(__dirname, 'client/build')));
-
-app.get("/api", (req, res) => {
-  res.sendFile(path.resolve(__dirname, '.../client/build', 'index.html'));
-});
+app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
 });
